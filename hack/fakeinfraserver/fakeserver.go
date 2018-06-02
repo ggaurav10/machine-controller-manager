3ac1f6bf78b746fe115b314f7728fec2ff7b5c7e
Gupta <gaurav.gupta07@sap.com>
2018-06-02 11:50:47 +0530

    gRPC framework for out-tree vendors

---
+++ hack/fakeinfraserver/fakeserver.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+
+	"net"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/gardener/machine-controller-manager/pkg/grpc/infrapb"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+// Server represents the gRPC server
+type Server struct {
+}
+
+// Create cretes an instance
+func (s *Server) Create(ctx context.Context, in *pb.CreateParams) (*pb.CreateResp, error) {
+	log.Printf("Receive message %s", in.Name)
+	return &pb.CreateResp{
+		ProviderID: "Create",
+	}, nil
+}
+
+// Delete deletes provided instance
+func (s *Server) Delete(ctx context.Context, in *pb.DeleteParams) (*pb.DeleteResp, error) {
+	log.Printf("Receive message %s", in.Name)
+	return &pb.DeleteResp{
+		Error: 0,
+	}, nil
+}
+
+// List returns list of instances
+func (s *Server) List(ctx context.Context, in *pb.ListParams) (*pb.ListResp, error) {
+	log.Printf("Receive message %s", in.Name)
+	return &pb.ListResp{
+		Message: "List",
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// Creates a new gRPC server
+	s := grpc.NewServer()
+	pb.RegisterInfragrpcServer(s, &Server{})
+	s.Serve(lis)
+}
---
+++ pkg/grpc/infraclient/infraclient.go
@@ -0,0 +1,80 @@
+package infraclient
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	pb "github.com/gardener/machine-controller-manager/pkg/grpc/infrapb"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+var (
+	// Address of grpc server
+	addr = "127.0.0.1:50051"
+
+	// API key
+	key = ""
+
+	// Authentication token
+	token = ""
+
+	// Path to a Google service account key file
+	keyfile = ""
+
+	// Audience
+	audience = ""
+)
+
+// Create makes grpc call to create instance
+func Create(name string) {
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewInfragrpcClient(conn)
+
+	if keyfile != "" {
+		log.Printf("Authenticating using Google service account key in %s", keyfile)
+		keyBytes, err := ioutil.ReadFile(keyfile)
+		if err != nil {
+			log.Fatalf("Unable to read service account key file %s: %v", keyfile, err)
+		}
+
+		tokenSource, err := google.JWTAccessTokenSourceFromJSON(keyBytes, audience)
+		if err != nil {
+			log.Fatalf("Error building JWT access token source: %v", err)
+		}
+		jwt, err := tokenSource.Token()
+		if err != nil {
+			log.Fatalf("Unable to generate JWT token: %v", err)
+		}
+		token = jwt.AccessToken
+		// NOTE: the generated JWT token has a 1h TTL.
+		// Make sure to refresh the token before it expires by calling TokenSource.Token() for each outgoing requests.
+		// Calls to this particular implementation of TokenSource.Token() are cheap.
+	}
+
+	ctx := context.Background()
+	if key != "" {
+		log.Printf("Using API key: %s", key)
+		ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("x-api-key", key))
+	}
+	if token != "" {
+		log.Printf("Using authentication token: %s", token)
+		ctx = metadata.NewOutgoingContext(ctx, metadata.Pairs("Authorization", fmt.Sprintf("Bearer %s", token)))
+	}
+
+	// Contact the server and print out its response.
+	r, err := c.Create(ctx, &pb.CreateParams{Name: name})
+	if err != nil {
+		log.Fatalf("could not create infra: %v", err)
+	}
+	log.Printf("infra: %s", r.ProviderID)
+}
